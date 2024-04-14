bc831c81921e30f2ba4b454f2965a0aa995ac18e
LongAloso <151179556+LongAloso@users.noreply.github.com>
2024-04-14 11:48:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 # To set a webpage title, header and subtitle
 st.set_page_config(page_title="Movies analysis", layout="wide")
 st.header("Interactive Dashboard")
